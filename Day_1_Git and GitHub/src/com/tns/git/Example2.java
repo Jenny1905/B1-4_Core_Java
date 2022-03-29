d6b83614435e24cd533b354733328b89af3c55ea
HP <HP@LAPTOP-KPAN81EK>
2022-03-29 18:34:15 +0530

    First Commit

---
+++ Day_1_Git and GitHub/src/com/tns/git/Example2.java
@@ -0,0 +1,12 @@
+package com.tns.git;
+
+public class Example2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Akansha Singh");
+		System.out.println("Akshay B");
+
+	}
+
+}

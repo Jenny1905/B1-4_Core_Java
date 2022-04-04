c1bd925a6a08e0a9ddcf94400693eab18f36cbc3
HP <HP@LAPTOP-KPAN81EK>
2022-04-04 19:39:32 +0530

    First Commit

---
+++ Day6/src/com/tns/abst/Example2.java
@@ -0,0 +1,27 @@
+package com.tns.abst;
+
+class Example11
+{
+	final void display()
+	{
+		System.out.println("Hello Students");
+	}
+}
+public class Example2 extends Example11
+{
+	 
+	
+	void eat()
+	{
+		
+		System.out.println("Welcome to C2TC");
+	}
+
+	public static void main(String[] args) 
+	{
+		Example2 e=new Example2();
+		e.display();
+
+	}
+
+}
---
+++ Day6/src/com/tns/abst/Example3.java
@@ -0,0 +1,26 @@
+package com.tns.abst;
+
+class A
+{
+	String name="Soujan";
+}
+class B extends A
+{
+	String name="Nikitha";
+	void print()
+	{
+		System.out.println("Hi, I am "+name);
+		System.out.println("Hi, I am "+super.name);
+	}
+}
+public class Example3 
+{
+
+	public static void main(String[] args) 
+	{
+		B b=new B();
+		b.print();
+
+	}
+
+}

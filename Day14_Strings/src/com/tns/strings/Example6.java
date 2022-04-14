b40965dc6fd9aca37a8247fd603e1ecd3ed68a4f
HP <HP@LAPTOP-KPAN81EK>
2022-04-14 19:20:38 +0530

    First Commit

---
+++ Day14_Strings/src/com/tns/strings/Example6.java
@@ -0,0 +1,11 @@
+package com.tns.strings;
+import java.util.StringTokenizer;  
+public class Example6 
+{
+	public static void main(String args[])
+	{  
+		StringTokenizer st = new StringTokenizer("Lets try this");  
+	    System.out.println(st.nextToken());  
+	    System.out.println(st.nextToken());
+	}  
+} 

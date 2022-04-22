db26e4c89e3602e4c1571062d9e683478c58763d
HP <HP@LAPTOP-KPAN81EK>
2022-04-22 18:50:06 +0530

    First Commit

---
+++ Day19_Junit_Testing/src/com/tns/junittesting/JUnitTesting4.java
@@ -0,0 +1,24 @@
+package com.tns.junittesting;
+
+
+import org.junit.jupiter.api.BeforeAll;
+
+
+import org.junit.jupiter.api.Test;
+
+
+
+public class JUnitTesting4 
+{
+	@BeforeAll
+	static void display()
+	{
+		System.out.println("Dileep");
+	}
+	
+	@Test
+	void display1()
+	{
+		System.out.println("Kamal");
+	}
+}
